9223ac8f59c4f15bb5bb107fa777f6f5e8382ff8
Ruben Vandamme <github-com-00ff86@vandamme.email>
2025-03-29 19:15:56 +0100

    Initial correct registration response
    Still contains hardcoded values

--- opensoho.go
+++ opensoho.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"log"
 	"fmt"
-	"net/http"
+	"log"
+	//"net/http"
 
 	"github.com/pocketbase/pocketbase"
 	//"github.com/pocketbase/pocketbase/apis"
@@ -34,13 +34,45 @@ func main() {
 			}{}
 			e.Response.Header().Set("X-Openwisp-Controller", "true")
 			if err := e.BindBody(&data); err != nil {
-				return e.BadRequestError("Failed to read request data", err)
+				return e.BadRequestError("Missing fields", err)
 			}
-			if(data.Secret == "blah"){
-				return e.BadRequestError("Not allowed", "")
+			if data.Secret != "blah" {
+				return e.BadRequestError("Registration failed!", "unrecognized secret")
 			}
-                        fmt.Print("Hello")
-			return e.JSON(http.StatusOK, map[string]bool{"success": true})
+			if data.Backend != "netjsonconfig.OpenWrt" {
+				return e.BadRequestError("Registration failed!", "wrong backend")
+			}
+			fmt.Print("Hello")
+			collection, err := app.FindCollectionByNameOrId("devices")
+			if err != nil {
+				return e.BadRequestError("Registration failed!", err)
+			}
+			record := core.NewRecord(collection)
+			record.Set("backend", data.Backend)
+			record.Set("key", data.Key)
+			record.Set("name", data.Name)
+			record.Set("hardware_id", data.HardwareId)
+			record.Set("mac_address", data.MacAddress)
+			record.Set("tags", data.Tags)
+			record.Set("model", data.Model)
+			record.Set("os", data.Os)
+			record.Set("system", data.System)
+			record.Set("last_ip_address", e.RealIP())
+			record.Set("health_status", "unknown")
+			record.Set("enabled", "true")
+			err = app.Save(record)
+			if err != nil {
+				return e.BadRequestError("Registration failed!", err)
+			}
+			isNew := 1
+			response := fmt.Sprintf(`registration-result: %s
+uuid: %s
+key: %s
+hostname: %s
+is-new: %d
+`, "success", "9f9d293c-be7c-4352-841c-240410f7e3c9", data.Key, data.Name, isNew)
+
+			return e.Blob(201, "text/plain", []byte(response))
 		})
 		return se.Next()
 	})

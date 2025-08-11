cc1be8d252a8c9b7b5e56e41ee47a148c4b4af9a
Ruben Vandamme <github-com-00ff86@vandamme.email>
2025-08-11 20:27:40 +0200

    Remove not-yet-implemented client steering method

---
+++ pb_migrations/1754936841_updated_client_steering.js
@@ -0,0 +1,41 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_3087215819")
+
+  // update field
+  collection.fields.addAt(4, new Field({
+    "hidden": false,
+    "id": "select1582905952",
+    "maxSelect": 1,
+    "name": "method",
+    "presentable": false,
+    "required": true,
+    "system": false,
+    "type": "select",
+    "values": [
+      "mac blacklist"
+    ]
+  }))
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_3087215819")
+
+  // update field
+  collection.fields.addAt(4, new Field({
+    "hidden": false,
+    "id": "select1582905952",
+    "maxSelect": 1,
+    "name": "method",
+    "presentable": false,
+    "required": true,
+    "system": false,
+    "type": "select",
+    "values": [
+      "mac blacklist",
+      "bss request (ieee80211v)"
+    ]
+  }))
+
+  return app.save(collection)
+})

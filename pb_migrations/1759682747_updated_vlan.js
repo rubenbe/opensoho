8b938bb8f4a2ceb195b7693f3d41398fe8a7c314
Ruben Vandamme <github-com-00ff86@vandamme.email>
2025-10-05 18:57:49 +0200

    Fix VLAN CIDR regex.

---
+++ pb_migrations/1759682747_updated_vlan.js
@@ -0,0 +1,42 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_3745276689")
+
+  // update field
+  collection.fields.addAt(2, new Field({
+    "autogeneratePattern": "",
+    "hidden": false,
+    "id": "text2445427222",
+    "max": 0,
+    "min": 0,
+    "name": "cidr",
+    "pattern": "^([0-9]{1,3}.){3}[0-9]{1,3}($|/([0-9]{1,2}))$",
+    "presentable": false,
+    "primaryKey": false,
+    "required": false,
+    "system": false,
+    "type": "text"
+  }))
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_3745276689")
+
+  // update field
+  collection.fields.addAt(2, new Field({
+    "autogeneratePattern": "",
+    "hidden": false,
+    "id": "text2445427222",
+    "max": 0,
+    "min": 0,
+    "name": "cidr",
+    "pattern": "^([0-9]{1,3}.){3}[0-9]{1,3}($|/(16|24))$",
+    "presentable": false,
+    "primaryKey": false,
+    "required": false,
+    "system": false,
+    "type": "text"
+  }))
+
+  return app.save(collection)
+})

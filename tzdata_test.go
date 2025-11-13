bf826fae1c4e370d6936c2ef7b346064a3dc4a50
Paul Donald <newtwen+github@gmail.com>
2025-11-13 20:56:37 +0100

    TZ: tzdb timezone names shall not contain spaces
    See e.g. https://en.wikipedia.org/wiki/List_of_tz_database_time_zones

--- tzdata_test.go
+++ tzdata_test.go
@@ -11,5 +11,5 @@ func TestGetTzData(t *testing.T) {
 	assert.Equal(t, "", GetTzData("Disabled"))
 	assert.Equal(t, "", GetTzData("UTC"))
 	assert.Equal(t, "CET-1CEST,M3.5.0,M10.5.0/3", GetTzData("Europe/Brussels"))
-	assert.Equal(t, "EST5EDT,M3.2.0,M11.1.0", GetTzData("America/New York"))
+	assert.Equal(t, "EST5EDT,M3.2.0,M11.1.0", GetTzData("America/New_York"))
 }

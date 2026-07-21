ac1f8b20b8ae549525f597d9f5126615efa7ec15
Ruben Vandamme <github-com-00ff86@vandamme.email>
2026-07-21 10:42:02 +0200

    Ethernet:add "unique" constraint on index

---
+++ pb_migrations/1784623299_updated_ethernet.js
@@ -0,0 +1,24 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_3702000743")
+
+  // update collection data
+  unmarshal({
+    "indexes": [
+      "CREATE UNIQUE INDEX `idx_E8lz3SFGI0` ON `ethernet` (\n  `device`,\n  `name`\n)"
+    ]
+  }, collection)
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_3702000743")
+
+  // update collection data
+  unmarshal({
+    "indexes": [
+      "CREATE INDEX `idx_E8lz3SFGI0` ON `ethernet` (\n  `device`,\n  `name`\n)"
+    ]
+  }, collection)
+
+  return app.save(collection)
+})
